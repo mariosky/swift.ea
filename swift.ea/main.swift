2a5b9c967abe7cb342240dd4947db05457f6203a
Mario Garcia Valdez <mariosky@gmail.com>
2016-04-16 00:02:46 -0700

    swift flip
    First version

--- swift.ea/main.swift
+++ swift.ea/main.swift
@@ -8,5 +8,48 @@
 
 import Foundation
 
-print("Hello, World!")
+let ITERATIONS = 100000;
+let MAXLENGTH:UInt32 = 32768;
+
+
+
+var length:UInt32 = 16;
+
+func randomBool() -> Bool {
+    return arc4random_uniform(2) == 0 ? true: false
+}
+
+
+
+
+repeat {
+    
+    var bits = [Bool]()
+    
+    for index in 1...length {
+                bits.append(randomBool());
+    }
+
+    
+    for i in 1...length {
+        bits.append(randomBool());
+    }
+    
+    let start = NSDate(); // <<<<<<<<<< Start time
+    
+    for i in 1...ITERATIONS{
+        let index = Int(arc4random_uniform(length))
+        bits [index] = !bits [index];
+    }
+    let end = NSDate();   // <<<<<<<<<<   end time
+    
+    
+     let timeInterval: Double = end.timeIntervalSinceDate(start);
+    
+    var str = NSString(format: "Swift Bool Array,%d,%.9f",length, timeInterval)
+    
+    print(str)
+    length = length*2;
+    
+} while length < MAXLENGTH
 
