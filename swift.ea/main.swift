e6eb3c1dba5c8400aea0b603e5df4f71aaf700a2
Mario Garcia Valdez <mariosky@gmail.com>
2016-04-15 23:11:00 -0700

    Initial Commit

---
+++ swift.ea/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swift.ea
+//
+//  Created by Mario Garcia Valdez on 4/15/16.
+//  Copyright © 2016 geneura. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
